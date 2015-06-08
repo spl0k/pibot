6a48f9653b2c7f3313943604041615bd1bfe6621
spl0k <alban.feron@gmail.com>
2015-06-08 22:20:59 +0200

    Replaced AJAX with Socket.IO

--- app.py
+++ app.py
@@ -2,17 +2,18 @@
 # coding: utf-8
 
 from flask import Flask, render_template, Response, request
+from flask.ext.socketio import SocketIO, emit
 import time
 from devices import Devices
 from camera import Camera
 from motors import Motors
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 Devices.set_classes([ Camera, Motors ])
 
 @app.route('/')
 def index():
-	Devices.startup()
 	return render_template('index.html')
 
 def generate_stream(cam):
@@ -38,16 +39,23 @@ def stream():
 	cam = Devices.get_device_instance(Camera)
 	return Response(generate_stream(cam), mimetype = 'multipart/x-mixed-replace; boundary=mjpegboundary')
 
-@app.route('/move', methods = [ 'POST' ])
-def move():
+@socketio.on('connect', namespace = '/cmd')
+def ws_connect():
+	Devices.startup()
+
+@socketio.on('disconnect', namespace = '/cmd')
+def ws_disconnect():
+	pass
+
+@socketio.on('move', namespace = '/cmd')
+def move(message):
 	commands = [
 		[ Motors.backward_left,  Motors.static_left,  Motors.forward_left  ],
 		[ Motors.backward,       Motors.stop,         Motors.forward       ],
 		[ Motors.backward_right, Motors.static_right, Motors.forward_right ]
 	]
-	x, y = map(lambda k: int(request.form[k]), [ 'x', 'y' ])
+	x, y = map(lambda k: int(message['data'][k]), [ 'x', 'y' ])
 	commands[x + 1][y + 1](Devices.get_device_instance(Motors))
-	return '{}'
 
 if __name__ == '__main__':
 	import argparse
@@ -56,8 +64,8 @@ if __name__ == '__main__':
 	argparser.add_argument('-h', '--host', default = '0.0.0.0')
 	argparser.add_argument('-p', '--port', type = int, default = 5000)
 	argparser.add_argument('-d', '--debug', action = 'store_true')
-	argparser.add_argument('-t', '--threaded', action = 'store_true')
 	args = argparser.parse_args()
 
-	app.run(host = args.host, port = args.port, debug = args.debug, threaded = args.threaded)
+	app.debug = args.debug
+	socketio.run(app, host = args.host, port = args.port)
 
